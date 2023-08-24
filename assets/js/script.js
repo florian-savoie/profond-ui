d2b0f64ff0e30673661a1d7f9cc37c747b2db50c
SampedroCyril <florianpsp303@gmail.com>
2023-08-24 15:02:21 +0200

    maj

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,40 +5,91 @@ document.addEventListener("DOMContentLoaded", function () {
     function toggleOpacity() {
         visible = !visible;
         clignotant.style.opacity = visible ? "1" : "0";
-        
+
     }
 
     setInterval(toggleOpacity, 800); // Change opacity every 1000 milliseconds (1 second)
 });
 
-if (document.getElementById('test')){
-    ajouterFeuilleDeStyle('https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css');
-    ajouterFeuilleDeStyle('https://floriansavoie.fr/assets/css/style.css');
-    ajouterScriptEndBody('https://code.jquery.com/jquery-3.6.0.min.js');
-    ajouterScriptHead('https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js');
+if (document.getElementById('theme1_64e32cde76bfa')) {
+    ajouterFeuilleDeStyle('https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900');
+    ajouterFeuilleDeStyle('https://florian-savoie.github.io/profond-ui/theme1/css/bootstrap.min.css');
+    ajouterFeuilleDeStyle('https://florian-savoie.github.io/profond-ui/theme1/css/font-awesome.min.css');
+    ajouterFeuilleDeStyle('https://florian-savoie.github.io/profond-ui/theme1/css/owl.carousel.css');
+    ajouterFeuilleDeStyle('https://florian-savoie.github.io/profond-ui/theme1/css/owl.theme.css');
+    ajouterFeuilleDeStyle('https://florian-savoie.github.io/profond-ui/theme1/css/owl.transitions.css');
+    ajouterFeuilleDeStyle('https://florian-savoie.github.io/profond-ui/theme1/css/meanmenu/meanmenu.min.css');
+    ajouterFeuilleDeStyle('https://florian-savoie.github.io/profond-ui/theme1/css/animate.css');
+    ajouterFeuilleDeStyle('https://florian-savoie.github.io/profond-ui/theme1/css/normalize.css');
+    ajouterFeuilleDeStyle('https://florian-savoie.github.io/profond-ui/theme1/css/scrollbar/jquery.mCustomScrollbar.min.css');
+    ajouterFeuilleDeStyle('https://florian-savoie.github.io/profond-ui/theme1/css/jvectormap/jquery-jvectormap-2.0.3.css');
+    ajouterFeuilleDeStyle('https://florian-savoie.github.io/profond-ui/theme1/css/notika-custom-icon.css');
+    ajouterFeuilleDeStyle('https://florian-savoie.github.io/profond-ui/theme1/css/wave/waves.min.css');
+    ajouterFeuilleDeStyle('https://florian-savoie.github.io/profond-ui/theme1/css/main.css');
+    ajouterFeuilleDeStyle('https://florian-savoie.github.io/profond-ui/theme1/style.css');
+    ajouterFeuilleDeStyle('https://florian-savoie.github.io/profond-ui/theme1/css/responsive.css');
+
+    ajouterScriptHead('https://florian-savoie.github.io/profond-ui/theme1/js/vendor/modernizr-2.8.3.min.js');
+
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/vendor/jquery-1.12.4.min.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/bootstrap.min.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/wow.min.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/jquery-price-slider.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/owl.carousel.min.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/jquery.scrollUp.min.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/meanmenu/jquery.meanmenu.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/counterup/jquery.counterup.min.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/counterup/waypoints.min.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/counterup/counterup-active.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/counterup/jquery.counterup.min.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/flot/jquery.flot.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/flot/jquery.flot.resize.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/flot/curvedLines.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/flot/flot-active.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/jvectormap/jquery-jvectormap-2.0.2.min.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/jvectormap/jquery-jvectormap-world-mill-en.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/jvectormap/jvectormap-active.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/scrollbar/jquery.mCustomScrollbar.concat.min.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/sparkline/jquery.sparkline.min.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/sparkline/sparkline-active.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/knob/jquery.knob.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/knob/jquery.appear.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/knob/knob-active.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/wave/waves.min.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/wave/wave-active.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/todo/jquery.todo.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/plugins.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/chat/moment.min.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/chat/jquery.chat.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/main.js');
+    ajouterScriptEndBody('https://florian-savoie.github.io/profond-ui/theme1/js/tawk-chat.js');
+
 }
 
-function ajouterFeuilleDeStyle(style) {
+
+
+
+    function ajouterFeuilleDeStyle(style) {
     const lienFeuilleDeStyle = document.createElement("link");
     lienFeuilleDeStyle.rel = "stylesheet";
-    lienFeuilleDeStyle.href = style; 
+    lienFeuilleDeStyle.href = style;
     document.head.appendChild(lienFeuilleDeStyle);
 }
 
-function ajouterScriptHead(script) {
+    function ajouterScriptHead(script) {
     const scriptElement = document.createElement("script");
-    scriptElement.src = script; 
+    scriptElement.src = script;
     document.head.appendChild(scriptElement);
 }
 
-function ajouterScriptEndBody(script) {
+    function ajouterScriptEndBody(script) {
     const scriptElement = document.createElement("script");
-    scriptElement.src = script; 
+    scriptElement.src = script;
     const baliseScriptExistante = document.querySelector("derniereDiv");
     document.body.insertBefore(scriptElement, baliseScriptExistante);
 }
 
-var structureHTML = `
+    var structureHTML = `
     <div class="container">
         <div class="row">
             <h1>Titre de niveau 1</h1>
@@ -49,5 +100,5 @@ var structureHTML = `
             <h6>Titre de niveau 6</h6>
         </div>
     </div>
-`;
-document.getElementById("HTML").innerHTML = structureHTML;
+    `;
+    document.getElementById("HTML").innerHTML = structureHTML;
